789d20a67dd442d48eee363259e8926452961360
Talha Learn AI <96698563+MuhammadTalha121@users.noreply.github.com>
2023-07-18 14:14:46 +0500

    matlab_formate.py

--- matlab_formate.py
+++ matlab_formate.py
@@ -1,10 +1,21 @@
 from scipy import io
 import numpy as np
 
-a = np.array([1, 2, 3, 4, 7, 9])
+array = np.array([1, 2, 3, 4, 7, 9])
 
-io.savemat('a.mat', {'vec': a})
 
-mdata = io.loadmat('a.mat')
+# The savemat() function allows us to export data in Matlab format.
+# Export the following array as variable name "vec" to a mat file:
+io.savemat('array.mat', {'vec': a})
 
-print(mdata)
+
+# The loadmat() function allows us to import data from a Matlab file.
+# Import the array from following mat file.:
+mydata = io.loadmat('array.mat')
+
+print(mydata)
+
+
+# Use the variable name "vec" to display only the array from the matlab data:
+
+print(mydata['vec'])
